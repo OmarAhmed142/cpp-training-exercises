cb5b4f2bd878e0344d6bb3a2646959460a4bb05d
OmarAhmed142 <omarahmedg979@gmail.com>
2025-08-05 02:11:29 +0300

    Loops-practice.cpp
    Implemented fundamental C++ loops — mastering for, while, and do-while! 🔁

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+  int count = 0;
+for (int i=1;i<=100;i++)
+
+if (i%7==0 )
+
+{
+ cout <<i<<endl ;
+count++;
+}
+cout << "N: "<<count<<endl ;
+
+
+
+
+
+
+}
