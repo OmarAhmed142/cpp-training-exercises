47a6556ecf80582fe83c0de78482f846c6882ffc
Omar <omarahmedg979@gmail.com>
2025-08-11 02:50:12 +0300

    Create For Loop – Task #3.cpp

---
+++ For Loop – Task #3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std ;
+int main() {
+    int N ;
+    cout << "Enter N "<<endl ;
+    cin >> N ;
+
+for (int i = 0 ; i <=N ;i++)    
+{
+int square = i*i ;
+cout <<i<<" the square is : "<<square<<endl;
+}
+    return 0;
+}
